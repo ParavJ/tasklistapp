ab98d65ad475e2c15953f5e70822ce28b816e7ef
Parav Jaikrishan <parav.jaikrishan@gmail.com>
2025-06-17 11:25:12 +0530

    Spring Boot backend setup

---
+++ tasklistapi/src/main/java/com/example/tasklistapi/TasklistapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasklistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklistapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklistapiApplication.class, args);
+	}
+
+}
---
+++ tasklistapi/src/test/java/com/example/tasklistapi/TasklistapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasklistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklistapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
